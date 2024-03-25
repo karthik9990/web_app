0a498aa8be138ad8394541efb1337cc91155ae5b
karthik9990 <karthik.tadakapalli@gmail.com>
2024-03-25 11:52:04 -0600

    Added a Page and renamed Web to Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -13,8 +15,10 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo App")
-st.write("this app is to increase productivity.")
+st.write("This app is to increase <b>productivity</b>.", unsafe_allow_html=True)
 
+st.text_input(label="", placeholder="Add new todo... ",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -24,8 +28,7 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo... ",
-              on_change=add_todo, key='new_todo')
+
 
 
 
